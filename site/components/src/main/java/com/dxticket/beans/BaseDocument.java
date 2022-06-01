59e33521158f39cf87b60333ff6248a9114547d5
Sezi Taser <sezitaser@gmail.com>
2022-06-01 16:31:14 +0200

    initial commit

---
+++ site/components/src/main/java/com/dxticket/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package com.dxticket.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="dxticket:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
