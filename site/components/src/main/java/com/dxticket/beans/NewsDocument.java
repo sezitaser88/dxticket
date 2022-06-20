e46f03ca2a428623e36aaa2974d6283ea7f4ab71
eeliese <elise.olthof@gmail.com>
2022-06-20 09:21:11 +0200

    Working frontend, only the container is missing to add components in the experience manager

---
+++ site/components/src/main/java/com/dxticket/beans/NewsDocument.java
@@ -0,0 +1,124 @@
+package com.dxticket.beans;
+/*
+ * Copyright 2014-2019 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import java.util.Calendar;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+import org.hippoecm.hst.content.beans.standard.HippoGalleryImageSet;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+
+@HippoEssentialsGenerated(internalName = "dxticket:newsdocument")
+@Node(jcrType="dxticket:newsdocument")
+public class NewsDocument extends HippoDocument {
+
+    /**
+     * The document type of the news document.
+     */
+    public static final String DOCUMENT_TYPE = "dxticket:newsdocument";
+
+    private static final String TITLE = "dxticket:title";
+    private static final String DATE = "dxticket:date";
+    private static final String INTRODUCTION = "dxticket:introduction";
+    private static final String IMAGE = "dxticket:image";
+    private static final String CONTENT = "dxticket:content";
+    private static final String LOCATION = "dxticket:location";
+    private static final String AUTHOR = "dxticket:author";
+    private static final String SOURCE = "dxticket:source";
+
+    /**
+     * Get the title of the document.
+     *
+     * @return the title
+     */
+    @HippoEssentialsGenerated(internalName = "dxticket:title")
+    public String getTitle() {
+        return getSingleProperty(TITLE);
+    }
+
+    /**
+     * Get the date of the document.
+     *
+     * @return the date
+     */
+    @HippoEssentialsGenerated(internalName = "dxticket:date")
+    public Calendar getDate() {
+        return getSingleProperty(DATE);
+    }
+
+    /**
+     * Get the introduction of the document.
+     *
+     * @return the introduction
+     */
+    @HippoEssentialsGenerated(internalName = "dxticket:introduction")
+    public String getIntroduction() {
+        return getSingleProperty(INTRODUCTION);
+    }
+
+    /**
+     * Get the image of the document.
+     *
+     * @return the image
+     */
+    @HippoEssentialsGenerated(internalName = "dxticket:image")
+    public HippoGalleryImageSet getImage() {
+        return getLinkedBean(IMAGE, HippoGalleryImageSet.class);
+    }
+
+    /**
+     * Get the main content of the document.
+     *
+     * @return the content
+     */
+    @HippoEssentialsGenerated(internalName = "dxticket:content")
+    public HippoHtml getContent() {
+        return getHippoHtml(CONTENT);
+    }
+
+    /**
+     * Get the location of the document.
+     *
+     * @return the location
+     */
+    @HippoEssentialsGenerated(internalName = "dxticket:location")
+    public String getLocation() {
+        return getSingleProperty(LOCATION);
+    }
+
+    /**
+     * Get the author of the document.
+     *
+     * @return the author
+     */
+    @HippoEssentialsGenerated(internalName = "dxticket:author")
+    public String getAuthor() {
+        return getSingleProperty(AUTHOR);
+    }
+
+    /**
+     * Get the source of the document.
+     *
+     * @return the source
+     */
+    @HippoEssentialsGenerated(internalName = "dxticket:source")
+    public String getSource() {
+        return getSingleProperty(SOURCE);
+    }
+
+}
+
